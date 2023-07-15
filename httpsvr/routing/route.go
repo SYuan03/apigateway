b07e0dc1282248adf21eaf7412f84f51b0bfd7bc
Jiao-Yiyang <1391079660@qq.com>
2023-07-15 21:20:44 +0800

    nothing

--- httpsvr/routing/route.go
+++ httpsvr/routing/route.go
@@ -26,7 +26,7 @@ func ProvideService(serviceName string, methodName string, c *app.RequestContext
 		return "", err
 	}
 	jsonString := string(jsonData)
-	//fmt.Println("Mes: ", jsonString)
+	fmt.Println("Mes: ", jsonString)
 	if err != nil {
 		c.String(consts.StatusBadRequest, err.Error())
 		return "", err

132520a98cc1964424905ebb2b664c4d249058f9
Jiao-Yiyang <1391079660@qq.com>
2023-07-15 16:26:32 +0800

    nihao

--- httpsvr/routing/route.go
+++ httpsvr/routing/route.go
@@ -3,26 +3,40 @@ package routing
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"nju/apigw/clientprovider"
 	"strings"
 )
 
 func ProvideService(serviceName string, methodName string, c *app.RequestContext, ctx context.Context) (string, error) {
-	data, err := json.Marshal(c.Request)
+	var data interface{}
+	err := json.Unmarshal(c.Request.Body(), &data)
 	if err != nil {
+		fmt.Println("Error:", err)
 		return "", err
 	}
 
-	dataString := string(data)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	jsonString := string(jsonData)
 
+	if err != nil {
+		c.String(consts.StatusBadRequest, err.Error())
+		return "", err
+	}
 	Cli := clientprovider.GetCli(serviceName)
-	resp, err := Cli.GenericCall(ctx, methodName, dataString)
+	resp, err := Cli.GenericCall(ctx, methodName, jsonString)
 	if err != nil {
 		return "", err
 	}
 
 	realResp := resp.(string)
+	//maybe
 	realResp = strings.ReplaceAll(realResp, "\"", "")
 	return realResp, nil
 }

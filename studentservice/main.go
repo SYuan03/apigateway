d44e8531678c0c2beb1fa3c56790646cf4db4e86
Jiao-Yiyang <1391079660@qq.com>
2023-07-15 14:55:28 +0800

    add studentservice for test

---
+++ studentservice/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/cloudwego/kitex/server"
+	"log"
+	"net"
+	"net/http"
+	"nju/apigw/kitex_gen/demo/studentservice"
+)
+
+func main() {
+	handler := &StudentServiceImpl{}
+	handler.InitDB()
+	addr, _ := net.ResolveTCPAddr("tcp", ":9990")
+	svr := studentservice.NewServer(handler, server.WithServiceAddr(addr))
+
+	err := svr.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+	go http.ListenAndServe("localhost:8880", nil)
+}

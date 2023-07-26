615a40543772628447072380980cbffae3d6bf48
ShengyuanD <211250097@smail.nju.edu.cn>
2023-07-26 22:50:53 +0800

    win win win

--- idl-managemet/main.go
+++ idl-managemet/main.go
@@ -6,17 +6,14 @@ import (
 	"github.com/cloudwego/hertz/pkg/app/server"
 
 	demo "github.com/SYuan03/idlmanage/biz/handler/demo"
-	
 )
 
 func main() {
 	// 初始化数据库
 	demo.InitDB()
 
-	h := server.New(server.WithHostPorts("127.0.0.1:6666"))
+	h := server.New(server.WithHostPorts(":6666"))
 
 	register(h)
 	h.Spin()
 }
-
-
